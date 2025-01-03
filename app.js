32496a75f324e6ac1e47d746bcfce92cd47879e4
Yadin Paulo Manrique Marquez <ypmanrique2@misena.edu.co>
2025-01-03 08:52:14 -0500

    correciones al registro

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const md5 = require('md5');
 const cors = require('cors');
 
 const corsOptions = {
-  origin: 'https://radiant-hummingbird-4a4f1e.netlify.app' || 'http://localhost:5173' ||  process.env.URLFRONTEND,
+  origin: 'https://radiant-hummingbird-4a4f1e.netlify.app' || 'http://localhost:5173' || process.env.URLFRONTEND,
   methods: ['GET', 'POST', 'DELETE', 'PUT'],  // Métodos permitidos
   allowedHeaders: ['Content-Type'],  // Cabeceras permitidas
   credentials: true
@@ -46,19 +46,18 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.post('/login', async (req, res) => {  // Cambiar de GET a POST
-  const { usuario, clave } = req.body;  // Obtener los datos del cuerpo
+app.post('/login', async (req, res) => {  
+  const { usuario, clave } = req.body;  
 
   try {
     const [results, fields] = await connection.query(
       "SELECT * FROM `usuarios` WHERE `usuario` = ? AND `clave` = ?",
-      [usuario, md5(clave)]  // Aquí utilizas MD5 para encriptar la contraseña
+      [usuario, md5(clave)]  
     );
     
     if (results.length > 0) {
-      req.session.usuario = usuario;  // Guardamos el usuario en la sesión
+      req.session.usuario = usuario;
 
-      // Verificamos el rol del usuario
       if (results[0].rol === 'ADMINISTRADOR') {
         req.session.administrador = true;
         return res.status(200).json({ rol: 'ADMINISTRADOR' });
@@ -66,13 +65,12 @@ app.post('/login', async (req, res) => {  // Cambiar de GET a POST
       
       return res.status(200).json({ rol: 'USUARIO' });
     } else {
-      res.status(401).send('Datos incorrectos');
+      return res.status(401).send('Datos incorrectos');
     }
   } catch (err) {
     console.log(err);
-    res.status(500).send('Error al iniciar sesión');
+    return res.status(500).send('Error al iniciar sesión');
   }
-  res.json({ success: true });
 });
 
 app.get('/validar', (req, res) => {
@@ -84,24 +82,32 @@ app.get('/validar', (req, res) => {
   res.json({ valid: true });
 })
 
-app.post('/registrar', (req, res) => {
-  console.log('Cuerpo de la solicitud:', req.body); // Verifica qué datos están llegando
-
-  // Verifica que los datos estén en el cuerpo de la solicitud
+app.post('/registrar', async (req, res) => {
+  const BASE_URL = process.env.NODE_ENV === 'production'
+    ? 'https://loginexpress-1-8pdh.onrender.com'  // URL del backend en producción
+    : 'http://localhost:10000';  // URL del backend en desarrollo
+  const peticion = await fetch(`${BASE_URL}/registrar`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      usuario: usuarioRegistro,
+      clave: claveRegistro,
+    }),
+  });
+  console.log('Cuerpo de la solicitud:', req.body); // Verifica qué datos están llegando en el cuerpo de la solicitud
   const { usuario, clave } = req.body;
   if (!usuario || !clave) {
     return res.status(400).send('Faltan datos en la solicitud');
   }
-
   console.log('Usuario recibido:', usuario);
-  
   // Inserta el nuevo usuario en la base de datos
   connection.query('INSERT INTO usuarios (usuario, clave) VALUES (?, ?)', [usuario, clave], (err, result) => {
     if (err) {
       console.log('Error en el registro:', err);
       return res.status(500).send('Error en el registro');
     }
-
     // Si todo va bien, responde con éxito
     res.status(200).send('Usuario registrado');
   });
